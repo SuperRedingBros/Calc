2703665a8a3e02c98a8668ff4b348460df94ca26
SuperRedingBros <redingtommy@gmail.com>
2021-09-23 08:34:29 -0400

    basic calc

---
+++ calc.js
@@ -0,0 +1,60 @@
+function p1() {
+  document.getElementById("display").innerHTML += "1"
+}
+function p2() {
+  document.getElementById("display").innerHTML += "2"
+}
+function p3() {
+  document.getElementById("display").innerHTML += "3"
+}
+function p4() {
+  document.getElementById("display").innerHTML += "4"
+}
+function p5() {
+  document.getElementById("display").innerHTML += "5"
+}
+function p6() {
+  document.getElementById("display").innerHTML += "6"
+}
+function p7() {
+  document.getElementById("display").innerHTML += "7"
+}
+function p8() {
+  document.getElementById("display").innerHTML += "8"
+}
+function p9() {
+  document.getElementById("display").innerHTML += "9"
+}
+function pplus() {
+  document.getElementById("display").innerHTML += "+"
+}
+function psub() {
+  document.getElementById("display").innerHTML += "-"
+}
+function pdiv() {
+  document.getElementById("display").innerHTML += "\\"
+}
+function pmul() {
+  document.getElementById("display").innerHTML += "*"
+}
+function pent() {
+  document.getElementById("display").innerHTML += "("
+}
+function pexit() {
+  document.getElementById("display").innerHTML += ")"
+}
+function pex() {
+  document.getElementById("display").innerHTML += "^"
+}
+function proot() {
+  document.getElementById("display").innerHTML += "√"
+}
+function pgo() {
+  document.getElementById("display").innerHTML = ""
+}
+function pabs() {
+  document.getElementById("display").innerHTML += "|"
+}
+function pclear() {
+  document.getElementById("display").innerHTML = ""
+}
