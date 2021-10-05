16e12597c52ee52d827d0379968496db8e017d9e
SuperRedingBros <redingtommy@gmail.com>
2021-10-05 07:58:41 -0400

    more stuff

--- calc.js
+++ calc.js
@@ -50,11 +50,84 @@ function proot() {
   document.getElementById("display").innerHTML += "√"
 }
 function pgo() {
-  document.getElementById("display").innerHTML = ""
+  formula = document.getElementById("display").innerHTML
+  solve(formula)
 }
 function pabs() {
   document.getElementById("display").innerHTML += "|"
 }
 function pclear() {
   document.getElementById("display").innerHTML = ""
+  n = 0
+}
+function pback() {
+  document.getElementById("display").innerHTML = document.getElementById("display").innerHTML.slice(0,-1)
+}
+function solve(f) {
+  //f = "(" + f + ")";
+  a = f.replace(/\)/g,"#");
+  a = a.replace(/\(/g,"#");
+  a = a.split("#");
+  //alert(a)
+  a.forEach((item, i) => {
+    solve2(item);
+  });
+
+}
+var n
+n = 0
+function solve2(i) {
+  i = i.replace(/\+/g,"#+");
+  i = i.replace(/\-/g,"#-");
+  i = i.replace(/\*/g,"#*");
+  i = i.replace(/\\/g,"#\\");
+  i = i.replace(/\√/g,"#√");
+  i = i.replace(/\^/g,"#^");
+  ia = i.split("#");
+  ia.forEach((item, i) => {
+    op = item[0];
+    switch (op) {
+      case "+":
+        //alert("+"+item.slice(1));
+        n += Number(item.slice(1));
+        break;
+      case "*":
+        //alert("+"+item.slice(1));
+        n = n * Number(item.slice(1));
+        break;
+      case "-":
+        //alert("+"+item.slice(1));
+        n -= Number(item.slice(1));
+        break;
+      case "\\":
+        //alert("+"+item.slice(1));
+        n = n / Number(item.slice(1));
+        break;
+      case "^":
+        alert("^ "+item.slice(1));
+        n = Math.pow( n , Number(item.slice(1)) );
+        break;
+      case "√":
+        //alert("+"+item.slice(1));
+        n = nthroot( Number(item.slice(1)) , n );
+        break;
+      default:
+        //alert("set "+item.slice(0));
+        n += Number(item.slice(0));
+    }
+  });
+  document.getElementById("display").innerHTML = n
+  n = 0
+}
+
+function nthroot(x, n) {
+  try {
+    var negate = n % 2 == 1 && x < 0;
+    if(negate)
+      x = -x;
+    var possible = Math.pow(x, 1 / n);
+    n = Math.pow(possible, n);
+    if(Math.abs(x - n) < 1 && (x > 0 == n > 0))
+      return negate ? -possible : possible;
+  } catch(e){}
 }
